9bb5d91398b5da46afc41397adc1c5aecb765692
Hackbright Student <info@hackbrightacademy.com>
2016-04-12 15:56:01 -0700

    reviewed and refined

--- markov.py
+++ markov.py
@@ -49,24 +49,22 @@ def make_text(chains):
 
     # initialize key bigram by selecting a random choice of chain key
     key_bigram = choice(chains.keys()) 
-    # initalize text by converting key bigram to the first string
-    text = str(key_bigram[0]) + " " + str(key_bigram[1])
+    # initalize text by joining strings in key bigram to make first string
+    text = " ".join(key_bigram)
 
     while key_bigram in chains:
         # get a random choice value from chains at the key_bigramth element
         random_value = choice(chains[key_bigram])
         # now text is equal to old text but new random value
         text = text + " " + random_value
-        # put text back into list to reference index for the following key
-        split_text = text.split()
         # generate new key bigram from last two words of previous text
-        key_bigram = (split_text[-2], split_text[-1])
+        key_bigram = (key_bigram[1], random_value)
 
     return text
 
 
-input_path = "green-eggs.txt"
-
+# input_path = "green-eggs.txt"
+input_path = "gettysburg.txt"
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
 
